308d84e9250a30e01bac987cc501ce7471d580a3
tYohannes <teklegy@gmail.com>
2022-02-28 20:11:16 +0300

    Render respective components for each route

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './Header.module.css';
 
-const NavBar = () => (
-  <nav className="navbar">
-    <ul className={classes.nav_list}>
-      <li className={classes.nav_item}>Rockets</li>
-      <li className={classes.nav_item}>Missions</li>
-      <li className={classes.nav_item}>My Profile</li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/myprofile',
+      text: 'My Profile',
+    },
+  ];
+  return (
+    <nav className="navbar">
+      <ul className={classes.nav_list}>
+        {
+          links.map((link) => (
+            <li key={link.id} className={classes.nav_item}>
+              <NavLink to={link.path}>
+                {link.text}
+              </NavLink>
+            </li>
+          ))
+        }
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const MyProfile = () => {
-  return (
-    <h2>MyProfile</h2>
-  )
-}
+const MyProfile = () => (
+  <h2>MyProfile</h2>
+);
 
 export default MyProfile;
