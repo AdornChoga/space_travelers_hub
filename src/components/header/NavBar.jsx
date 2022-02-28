5f9cd973d7cd526104d759f0ba0e1d9a986c9cd8
tYohannes <teklegy@gmail.com>
2022-02-28 17:09:48 +0300

    Add NavBar component

---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li>Rockets</li>
+      <li>Missions</li>
+      <li>My Profile</li>
+    </ul>
+  )
+}
+
+export default NavBar
