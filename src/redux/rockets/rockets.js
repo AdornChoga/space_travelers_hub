12ce65cc49707b6cdd7362d582bb2b8760551dbe
asanchy <sanchyclips@gmail.com>
2022-03-01 07:37:24 +0200

    Create rockets state initialize action and rockets reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,3 +1,14 @@
-const rocketsReducer = () => (5 + 5);
+const ACTIONS = {
+  INITIALIZE: 'rockets/state/initialize',
+};
+
+const rocketsReducer = (state = [], action) => {
+  switch (action.type) {
+    case ACTIONS.INITIALIZE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default rocketsReducer;
