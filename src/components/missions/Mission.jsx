621376d90c60646303b0cb0bc8bf095bef9924f1
tYohannes <teklegy@gmail.com>
2022-03-01 12:08:08 +0300

    Add dispatch joinMission action handler on join mission button click event

--- src/components/missions/Mission.jsx
+++ src/components/missions/Mission.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 import classes from './MissionsList.module.css';
+import { joinMission } from '../../redux/missions/missions';
 
-const Mission = ({ mission }) => (
-  <li id={mission.mission_id}>
-    <div className={classes.mission_name}>{mission.mission_name}</div>
-    <div className={classes.mission_description}>{mission.description}</div>
-    <div className={classes.mission_status}><p>Not A Member</p></div>
-    <div className={classes.mission_join_leave}><button type="button">Join Mission</button></div>
-  </li>
-);
+const Mission = ({ mission }) => {
+  const dispatch = useDispatch();
+
+  const joinMissionHandler = (e) => {
+    const { id } = e.target.parentNode.parentNode;
+    dispatch(joinMission({ id }));
+  };
+
+  return (
+    <li id={mission.mission_id}>
+      <div className={classes.mission_name}>{mission.mission_name}</div>
+      <div className={classes.mission_description}>{mission.description}</div>
+      <div className={classes.mission_status}><p>Not A Member</p></div>
+      <div className={classes.mission_join_leave}><button type="button" onClick={joinMissionHandler}>Join Mission</button></div>
+    </li>
+  );
+};
 
 Mission.propTypes = {
   mission: PropTypes.shape({
