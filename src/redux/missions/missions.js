4f3b8a0e8507283c4a78ce15539d55c64bd61f91
tYohannes <teklegy@gmail.com>
2022-03-01 12:04:07 +0300

    Add joinMission action creator

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://api.spacexdata.com/v3';
 const FETCH_SUCCESS = 'missions/missionsFetched';
 const FETCH_FAIL = 'missions/missionsFetchFailed';
+const JOIN_MISSION = 'missions/missionsJoined';
 
 const initialState = [];
 
@@ -25,12 +26,22 @@ export const fetchMissions = () => async (dispatch) => {
   }
 };
 
+export const joinMission = (payload) => ({
+  type: JOIN_MISSION,
+  payload,
+});
+
 const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_SUCCESS:
       return [...action.payload];
     case FETCH_FAIL:
       return [];
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id === action.payload.id) return { ...mission, reserved: true };
+        return mission;
+      });
     default:
       return state;
   }
