3ef77819e1d98537a0b0bc329d9d60a74f3859da
asanchy <sanchyclips@gmail.com>
2022-03-01 10:15:51 +0200

    Add condition for fetching rockets only once

--- src/components/rockets/Rockets.jsx
+++ src/components/rockets/Rockets.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import { initializeState } from '../../redux/rockets/rockets';
 
 const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
   useEffect(async () => {
-    const { data } = await axios.get('https://api.spacexdata.com/v3/rockets');
-    const rockets = data.map((rocket) => (
-      {
-        id: rocket.id,
-        rocket_name: rocket.rocket_name,
-        description: rocket.description,
-        flickr_images: rocket.flickr_images[0],
-      }
-    ));
-    dispatch(initializeState(rockets));
+    if (rockets.length === 0) {
+      const { data } = await axios.get('https://api.spacexdata.com/v3/rockets');
+      const rockets = data.map((rocket) => (
+        {
+          id: rocket.id,
+          rocket_name: rocket.rocket_name,
+          description: rocket.description,
+          flickr_images: rocket.flickr_images[0],
+        }
+      ));
+      dispatch(initializeState(rockets));
+    }
   }, []);
   return (
     <h1>Rockets</h1>
