3cb5779a3ea78c9150f32f5b1a49b97dca0f23e8
asanchy <sanchyclips@gmail.com>
2022-03-01 10:25:21 +0200

    Moved the rockets-fetching code to the state initializer action creator

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,11 +1,21 @@
+import axios from 'axios';
+
 const ACTIONS = {
   INITIALIZE: 'rockets/state/initialize',
 };
 
-const initializeState = (payload) => ({
-  type: ACTIONS.INITIALIZE,
-  payload,
-});
+const initializeState = () => async (dispatch) => {
+  const { data } = await axios.get('https://api.spacexdata.com/v3/rockets');
+  const rockets = data.map((rocket) => (
+    {
+      id: rocket.id,
+      rocket_name: rocket.rocket_name,
+      description: rocket.description,
+      flickr_images: rocket.flickr_images[0],
+    }
+  ));
+  dispatch({ type: ACTIONS.INITIALIZE, payload: rockets });
+};
 
 const rocketsReducer = (state = [], action) => {
   switch (action.type) {
